1393d32e655a98de62257a1d9f8caefb96c3d13d
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 01:03:13 +0530

    Add BlogSidebar component to display recent posts and popular resources

---
+++ src/components/BlogSidebar/BlogSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogSidebar = () => {
+  return (
+    <div className="blog-sidebar">
+      <h3>Recent Posts</h3>
+      <p>Popular articles and resources</p>
+    </div>
+  );
+};
+
+export default BlogSidebar;
