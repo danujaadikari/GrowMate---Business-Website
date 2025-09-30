15612fb03210d2af60edc70cd8a6227320e35af9
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 01:01:55 +0530

    Add About component to provide information about GrowMate and its mission

---
+++ src/components/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="about-section section-padding">
+      <div className="container">
+        <div className="about-content">
+          <div className="about-text">
+            <h2 className="heading-lg">About GrowMate</h2>
+            <p className="body-lg text-secondary">
+              We are a team of passionate professionals dedicated to helping businesses achieve sustainable growth.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
