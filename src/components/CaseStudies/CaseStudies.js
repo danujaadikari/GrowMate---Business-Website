a4fcb414a57f22694457d73b8645292bfccd86ef
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 01:02:20 +0530

    Add CaseStudies component to showcase client success stories

---
+++ src/components/CaseStudies/CaseStudies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CaseStudies = () => {
+  return (
+    <section className="section-padding bg-secondary">
+      <div className="container text-center">
+        <h2 className="heading-lg">Case Studies</h2>
+        <p className="body-lg text-secondary">Detailed success stories from our clients</p>
+      </div>
+    </section>
+  );
+};
+
+export default CaseStudies;
