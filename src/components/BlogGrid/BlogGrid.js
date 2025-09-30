218dea2dadb580d5503fc9e1b40f13924aa7eb4c
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 01:02:08 +0530

    Add BlogGrid component to display latest articles and insights

---
+++ src/components/BlogGrid/BlogGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogGrid = () => {
+  return (
+    <div className="blog-grid">
+      <h2 className="heading-lg">Latest Articles</h2>
+      <p className="body-lg text-secondary">Stay updated with our latest insights and tips</p>
+    </div>
+  );
+};
+
+export default BlogGrid;
