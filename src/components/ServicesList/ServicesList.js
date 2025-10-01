73c0d63bbe0af01c4762ae73651233272812377d
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:27:37 +0530

    Add ServicesList component to display service offerings

---
+++ src/components/ServicesList/ServicesList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ServicesList = () => {
+  return (
+    <section className="section-padding">
+      <div className="container text-center">
+        <h2 className="heading-lg">Our Services</h2>
+        <p className="body-lg text-secondary">Comprehensive solutions for your business needs</p>
+      </div>
+    </section>
+  );
+};
+
+export default ServicesList;
