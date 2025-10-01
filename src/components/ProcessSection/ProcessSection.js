1a031829745f547d8659380e6073e7c46880a2d3
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:26:54 +0530

    Add ProcessSection component to outline business delivery process

---
+++ src/components/ProcessSection/ProcessSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProcessSection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container text-center">
+        <h2 className="heading-lg">Our Process</h2>
+        <p className="body-lg text-secondary">How we deliver results for your business</p>
+      </div>
+    </section>
+  );
+};
+
+export default ProcessSection;
