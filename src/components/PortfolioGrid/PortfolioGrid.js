9e7b3e9ee7a99c14e03caac293bb441641085fdf
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:26:37 +0530

    Add PortfolioGrid component to display project overview

---
+++ src/components/PortfolioGrid/PortfolioGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PortfolioGrid = () => {
+  return (
+    <section className="section-padding">
+      <div className="container text-center">
+        <h2 className="heading-lg">Portfolio Grid</h2>
+        <p className="body-lg text-secondary">Explore our successful projects</p>
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioGrid;
