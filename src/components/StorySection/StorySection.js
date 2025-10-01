e59f9e25cad1b5a5dd4be9fc417ca9956ebb367f
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:27:51 +0530

    Add StorySection component to showcase the company's narrative and mission

---
+++ src/components/StorySection/StorySection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StorySection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container text-center">
+        <h2 className="heading-lg">Our Story</h2>
+        <p className="body-lg text-secondary">How GrowMate began and our mission to help businesses thrive</p>
+      </div>
+    </section>
+  );
+};
+
+export default StorySection;
