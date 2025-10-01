b8bdcd5375867735ef9dcf598adb686650323b81
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:28:54 +0530

    Add TeamSection component to introduce the team behind the business

---
+++ src/components/TeamSection/TeamSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TeamSection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container text-center">
+        <h2 className="heading-lg">Our Team</h2>
+        <p className="body-lg text-secondary">Meet the experts behind your success</p>
+      </div>
+    </section>
+  );
+};
+
+export default TeamSection;
