8a28e8b014b4c112c54b90f4d4062ca2f836f58d
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:26:10 +0530

    Add PricingSection component to display pricing plans and descriptions

---
+++ src/components/PricingSection/PricingSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PricingSection = () => {
+  return (
+    <section className="section-padding bg-secondary">
+      <div className="container text-center">
+        <h2 className="heading-lg">Pricing Plans</h2>
+        <p className="body-lg text-secondary">Choose the plan that fits your business needs</p>
+      </div>
+    </section>
+  );
+};
+
+export default PricingSection;
