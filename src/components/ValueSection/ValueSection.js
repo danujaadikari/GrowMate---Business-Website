18120304d27a574eac316a20a5098c9b2bd03e60
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-01 07:29:19 +0530

    Add ValueSection component to highlight core values of the business

---
+++ src/components/ValueSection/ValueSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ValueSection = () => {
+  return (
+    <section className="section-padding bg-secondary">
+      <div className="container text-center">
+        <h2 className="heading-lg">Our Values</h2>
+        <p className="body-lg text-secondary">What drives us to deliver excellence</p>
+      </div>
+    </section>
+  );
+};
+
+export default ValueSection;
