7852118757bdb99b5fab2769670e932896d4b852
Danuja Vimodya <danujaadikari2001@gmail.com>
2025-10-06 21:02:11 +0530

    Add DevelopmentProcess component to Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import Services from '../../components/Services/Services';
+import DevelopmentProcess from '../../components/Development/DevelopmentProcess';
 import About from '../../components/About/About';
 import Portfolio from '../../components/Portfolio/Portfolio';
 import Testimonials from '../../components/Testimonials/Testimonials';
@@ -12,6 +13,7 @@ const Home = () => {
     <div className="page-wrapper">
       <Hero />
       <Services />
+  <DevelopmentProcess />
       <About />
       <Portfolio />
       <Testimonials />
